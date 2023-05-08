49b8867d794b7f173f61c45597cb0d2aae0888c2
Marcelo Henrique De Moura <marcelohmoura99@outlook.com.br>
2023-05-08 06:12:43 -0300

    exercicio: Soma de números inteiros

---
+++ exercicios/soma-numeros-inteiros/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 2;
+        int soma = n1 + n2;
+
+        System.out.println("O Resultado da soma entre dois números é: " + soma);
+    }
+}
