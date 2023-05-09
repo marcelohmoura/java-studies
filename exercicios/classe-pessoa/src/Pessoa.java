eec02371d03249b455dd0bfc0c2e3c9dfdc55a43
Marcelo Henrique De Moura <marcelohmoura99@outlook.com.br>
2023-05-09 05:58:15 -0300

    Criando Classe Pessoa

---
+++ exercicios/classe-pessoa/src/Pessoa.java
@@ -0,0 +1,23 @@
+public class Pessoa {
+    String nome, endereco;
+    int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
