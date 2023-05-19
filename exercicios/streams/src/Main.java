d51d91ec7a9ac189117f5be012574aa69720af52
Marcelo Henrique De Moura <marcelohmoura99@outlook.com.br>
2023-05-19 07:16:04 -0300

    Mais exercícios utilizando a Stream API

--- exercicios/streams/src/Main.java
+++ exercicios/streams/src/Main.java
@@ -1,7 +1,6 @@
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     class Str {
@@ -11,6 +10,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
+
         // Ordena Strings da menor para a maior.
 
         List<String> lista = Arrays.asList("abcd", "a", "abc", "ab");
@@ -20,5 +20,38 @@ public class Main {
 
         System.out.println(listaOrdenada);
 
+        /////////////////////////////////////////////
+
+        // Cálculo da soma de números pares de uma matriz
+
+        int[][] matriz = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+        int soma = Arrays.stream(matriz)
+                .flatMapToInt(IntStream::of)
+                .filter(e -> e % 2 == 0)
+                .sum();
+
+        System.out.println("Soma dos números pares da matriz: " + soma);
+
+        //////////////////////////////////////////////////////////////////
+
+        // Encontra o valor máximo em um Array usando Streams
+
+        int[] arr = {1, 2, 4, 3, 5, 6, 10, 20, 31, 12};
+        OptionalInt max = Arrays.stream(arr).max();
+
+        System.out.println("Valor máximo: " + 31);
+
+        ///////////////////////////////////////////////////////////////////
+
+        // Coloca todas as Strings de um Array em caixa alta
+
+        String[] arrOfStrings = {"marcelo", "henrique", "de", "moura"};
+        List<String> itensEmCaixaAlta = Arrays
+                .stream(arrOfStrings)
+                .map(String::toUpperCase)
+                .toList();
+
+        System.out.println("Em caixa alta: " + itensEmCaixaAlta);
     }
 }
